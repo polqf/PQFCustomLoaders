c97d10b9a8ed6f8ad20387ed2207f1e6ecaba140
Pol Quintana <polkintana@gmail.com>
2014-11-02 16:39:59 +0100

    Updated Demo

--- Demo/PQFCustomLoaders/ViewController.m
+++ Demo/PQFCustomLoaders/ViewController.m
@@ -13,6 +13,7 @@
 @interface ViewController ()
 
 @property (nonatomic, strong) PQFBarsInCircle *loader;
+@property (weak, nonatomic) IBOutlet UITextField *textField;
 
 @end
 
@@ -22,10 +23,25 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     self.view.backgroundColor = [UIColor flatTurquoiseColor];
-    
+}
+
+- (IBAction)showBarsInCircle:(id)sender {
+    [self.loader remove];
+
     self.loader = [[PQFBarsInCircle alloc] initLoaderOnView:self.view];
+    self.loader.label.text = self.textField.text;
     
     [self.loader show];
+}
+
+- (IBAction)showBouncingBalls:(id)sender {
+    [self.loader remove];
+    
+}
+
+- (IBAction)showCirclesInTriangles:(id)sender {
+    [self.loader remove];
+
 }
 
 - (IBAction)removeLoader:(id)sender {
